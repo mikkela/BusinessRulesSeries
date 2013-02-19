efd1af28add31ffdb5c7030243524fd3cba1f125
Mikkel Arentoft <mikkel@mikadocs.com>
2013-02-19 20:23:45 +0100

    Created Reason Based interest calculator

---
+++ Business/ReasonBasedInterestCalculator.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Business
+{
+    public class ReasonBasedInterestCalculator
+    {
+        public Tuple<string, IBusinessRule> GetReasonForInterest(Tuple<int?, IBusinessRule> interest)
+        {
+            var reasonMap = new[]
+                {
+                    new {Rule = typeof (UnderAgedBusinessRule), Reason = "Because you are too young"},
+                    new {Rule = typeof (YoungBusinessRule), Reason = "Because you will party before you pay"},
+                    new {Rule = typeof (MiddleAgedBusinessRule), Reason = "Because you got family - we get security"},
+                    new {Rule = typeof (OldBusinessRule), Reason = "Because you are old"}
+                };
+
+            return
+                new Tuple<string, IBusinessRule>(
+                    reasonMap.Where(p => p.Rule == interest.Item2.GetType()).Select(p => p.Reason).First(),
+                    interest.Item2);
+        }
+
+        public Tuple<int?, IBusinessRule> CalculateInterestRate(int age)
+        {
+            Tuple<bool, IBusinessRule> isApplicantAccepted = IsApplicantAccepted(age);
+            if (!isApplicantAccepted.Item1)
+                return new Tuple<int?, IBusinessRule>(null, isApplicantAccepted.Item2);
+
+            var interestMap = new[]
+                {
+                    new {Rule = typeof (YoungBusinessRule), Rate = 25},
+                    new {Rule = typeof (MiddleAgedBusinessRule), Rate = 15},
+                    new {Rule = typeof (OldBusinessRule), Rate = 20}
+                };
+
+            return
+                new Tuple<int?, IBusinessRule>(
+                    interestMap.Where(p => p.Rule == isApplicantAccepted.Item2.GetType()).Select(p => p.Rate).First(),
+                    isApplicantAccepted.Item2);
+        }
+
+        public Tuple<bool, IBusinessRule> IsApplicantAccepted(int age)
+        {
+            var acceptRules = new IBusinessRule[]
+                {new YoungBusinessRule(age), new MiddleAgedBusinessRule(age), new OldBusinessRule(age)};
+            var rejectRules = new IBusinessRule[] {new UnderAgedBusinessRule(age),};
+
+            return EvaluateStatement(acceptRules, rejectRules);
+        }
+
+        private Tuple<bool, IBusinessRule> EvaluateStatement(IEnumerable<IBusinessRule> acceptRules,
+                                                             IEnumerable<IBusinessRule> rejectRules)
+        {
+            IBusinessRule acceptMatch = acceptRules.FirstOrDefault(p => p.Evaluate());
+            if (acceptMatch != null)
+                return new Tuple<bool, IBusinessRule>(true, acceptMatch);
+            IBusinessRule rejectMatch = rejectRules.FirstOrDefault(p => p.Evaluate());
+            if (rejectMatch != null)
+                return new Tuple<bool, IBusinessRule>(false, rejectMatch);
+
+            throw new ArgumentException("No rules accepting or rejecting the statement");
+        }
+    }
+}
---
+++ BusinessTest/ReasonBasedInterestCalculatorTest.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Business;
+using Xunit;
+using Xunit.Extensions;
+
+namespace BusinessTest
+{
+    public class ReasonBasedInterestCalculatorTest : TestBase
+    {
+        [Theory]
+        [PropertyData("UnderAge")]
+        public void The_interest_rate_of_a_under_age_person_is_null(int age)
+        {
+            Assert.Null(new ReasonBasedInterestCalculator().CalculateInterestRate(age).Item1);
+        }
+
+        [Theory]
+        [PropertyData("Young")]
+        public void The_interest_rate_of_a_young_person_is_25(int age)
+        {
+            Assert.Equal(25, new ReasonBasedInterestCalculator().CalculateInterestRate(age).Item1);
+        }
+
+        [Theory]
+        [PropertyData("MiddleAged")]
+        public void The_interest_rate_of_a_middle_aged_person_is_15(int age)
+        {
+            Assert.Equal(15, new ReasonBasedInterestCalculator().CalculateInterestRate(age).Item1);
+        }
+
+        [Theory]
+        [PropertyData("Old")]
+        public void The_interest_rate_of_an_old_person_is_20(int age)
+        {
+            Assert.Equal(20, new ReasonBasedInterestCalculator().CalculateInterestRate(age).Item1);
+        }
+
+        [Theory]
+        [PropertyData("UnderAge")]
+        public void The_reason_for_interest_rate_of_a_under_age_person_is_as_defined(int age)
+        {
+            var target = new ReasonBasedInterestCalculator();
+            Assert.Equal("Because you are too young", new ReasonBasedInterestCalculator().GetReasonForInterest(target.CalculateInterestRate(age)).Item1);
+        }
+
+        [Theory]
+        [PropertyData("Young")]
+        public void The_reason_for_interest_rate_of_a_young_person_is_as_defined(int age)
+        {
+            var target = new ReasonBasedInterestCalculator();
+            Assert.Equal("Because you will party before you pay", new ReasonBasedInterestCalculator().GetReasonForInterest(target.CalculateInterestRate(age)).Item1);
+        }
+
+        [Theory]
+        [PropertyData("MiddleAged")]
+        public void The_reason_for_interest_rate_of_a_middle_aged_person_is_as_defined(int age)
+        {
+            var target = new ReasonBasedInterestCalculator();
+            Assert.Equal("Because you got family - we get security", new ReasonBasedInterestCalculator().GetReasonForInterest(target.CalculateInterestRate(age)).Item1);
+        }
+
+        [Theory]
+        [PropertyData("Old")]
+        public void The_reason_for_interest_rate_of_an_old_person_is_as_defined(int age)
+        {
+            var target = new ReasonBasedInterestCalculator();
+            Assert.Equal("Because you are old", new ReasonBasedInterestCalculator().GetReasonForInterest(target.CalculateInterestRate(age)).Item1);
+        }
+    }
+}
